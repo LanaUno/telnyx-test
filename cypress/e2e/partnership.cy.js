7da9ca77040725f9e69d5ab6e628acf5b33894ac
Svitlana Andrushenko <wwwli4e@gmail.com>
2025-03-25 16:24:11 +0200

    feat: change files

--- cypress/e2e/partnership.cy.js
+++ cypress/e2e/partnership.cy.js
@@ -1,10 +1,8 @@
 /// <reference types="cypress" />
 import { MainPage } from "../pageObjects/main.page";
-import { ThanksPage } from "../pageObjects/thanks.page";
 import { PartnerPage } from "../pageObjects/partner.page";
 
 const mainPage = new MainPage();
-const thanksPage = new ThanksPage();
 const partnerPage = new PartnerPage();
 
 describe("Telnyx testing", () => {
@@ -12,22 +10,7 @@ describe("Telnyx testing", () => {
     cy.visit("https://telnyx.com/");
   });
 
-  it("Become a Partner", () => {
-    mainPage.clickWhyTelnyxBtn();
-    mainPage.clickPartnersLink();
-    cy.scrollTo('0%','75%')
-    cy.wait(8000)
-    partnerPage.typeFirstName({waitForAnimations: false});
-    partnerPage.typeLastName();
-    partnerPage.typeCompanyName();
-    partnerPage.typeBusinessEmail();
-    partnerPage.selectCountry();
-    partnerPage.typePhoneNumber();
-    partnerPage.getPartnerTypeOption();
-    partnerPage.typeAddInfoRequest();
-    partnerPage.clickSubmitButton();
-    thanksPage.getThanksMessage().should("have.text", "Thank you.");
-  });
+
 
   it("Too long Phone Number in Become a Partner Form", () => {
     mainPage.clickWhyTelnyxBtn();
